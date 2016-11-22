c6b28d712679ba89211da065bfa3d1f1a95905df
bin.zhang <bin.zhang@shuyun.com>
2016-11-22 21:02:38 +0800

    add projOne to github

---
+++ projOne/src/main/java/bin/zhang/projOne/App.java
@@ -0,0 +1,13 @@
+package bin.zhang.projOne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
